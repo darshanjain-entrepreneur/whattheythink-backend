ae456de53a035b0abad31d5a300e67af392abbac
Darshan <darshanjain.godha@gmail.com>
2025-12-31 16:59:23 +0530

    Add hosted privacy policy

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
